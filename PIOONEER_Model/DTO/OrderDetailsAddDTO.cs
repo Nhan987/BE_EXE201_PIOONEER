34d4601dd67a71e7b7d2ce14dff514cbfb2d6a25
Nhan987 <132928751+Nhan987@users.noreply.github.com>
2024-05-26 10:46:31 +0700

    crud for product  ,order and order details
    crud for product  ,order and order details , check valide

---
+++ PIOONEER_Model/DTO/OrderDetailsAddDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIOONEER_Model.DTO
+{
+    public class OrderDetailsAddDTO
+    {
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "ProductId must be a number.")]
+        public long ProductId { get; set; }
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "OrderId must be a number.")]
+        public long OrderId { get; set; }
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "OrderQuantity must be a number.")]
+        public int OrderQuantity { get; set; }
+        [Required]
+        [RegularExpression(@"^-?\d+(\.\d+)?$", ErrorMessage = "TotalPrice description must be a valid float number.")]
+        public double OrderPrice { get; set; }
+    }
+}
---
+++ PIOONEER_Model/DTO/OrderDetailsUpDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIOONEER_Model.DTO
+{
+    public class OrderDetailsUpDTO
+    {
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = "= must be a number not float.")]
+        public int OrderQuantity { get; set; }
+        [Required]
+        [Range(0, int.MaxValue, ErrorMessage = " must be a number.")]
+        public double OrderPrice { get; set; }
+    }
+}
