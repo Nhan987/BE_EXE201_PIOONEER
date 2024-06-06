971a85b8fddad47e45d89ea52c315e0bf47aab3b
Nhan987 <132928751+Nhan987@users.noreply.github.com>
2024-06-06 21:19:36 +0700

    Update Program.cs

--- PIOONEER_API/Program.cs
+++ PIOONEER_API/Program.cs
@@ -1,18 +1,37 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using PIOONEER_Model.Mapper;
 using PIOONEER_Repository.Entity;
 using PIOONEER_Repository.Repository;
+<<<<<<< HEAD
 using Tools;
 
+=======
+using PIOONEER_Service.Interface;
+using PIOONEER_Service.Service;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+>>>>>>> main
 var builder = WebApplication.CreateBuilder(args);
-
+    
 
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+<<<<<<< HEAD
 builder.Services.AddScoped<Firebases>();
+=======
+
+
+///
+builder.Services.AddScoped<ILoginService, LoginService>();
+builder.Services.AddScoped<IUserService, UserService>();
+///
+>>>>>>> main
 var config = new MapperConfiguration(cfg =>
 {
     cfg.AddProfile(new AutoMapperProfile());
@@ -26,6 +45,48 @@ builder.Services.AddDbContext<MyDbContext>(options =>
     options.UseMySql(connectionString, serverVersion, options => options.MigrationsAssembly("PIOONEER_API"));
 }
 );
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+
+// Add authorization policies
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+});
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
