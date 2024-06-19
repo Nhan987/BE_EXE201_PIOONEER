b3743e34b3a89cc2e8287d4e01e61844be3135ce
Chu Tuấn Kiệt <105277832+kaolaa20@users.noreply.github.com>
2024-06-19 12:38:33 +0700

    fix unuse fiead

--- PIOONEER_Repository/Entity/User.cs
+++ PIOONEER_Repository/Entity/User.cs
@@ -22,7 +22,6 @@ namespace PIOONEER_Repository.Entity
         public string Password { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public string PhoneNumber { get; set; }
         public string Address { get; set; }
 
         public string? PhoneNumber { get; set; }
