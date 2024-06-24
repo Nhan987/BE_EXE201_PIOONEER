8e8a7e40478a9011a6c72ca551d0c81418a0a358
Chu Tuấn Kiệt <105277832+kaolaa20@users.noreply.github.com>
2024-06-24 12:25:05 +0700

    Fix unuse migration and missing field

--- PIOONEER_Repository/Entity/User.cs
+++ PIOONEER_Repository/Entity/User.cs
@@ -23,7 +23,7 @@ namespace PIOONEER_Repository.Entity
         public string Name { get; set; }
         public string Email { get; set; }
         public string Address { get; set; }
-
+        public string PhoneNumber { get; set; }
         public long RoleId { get; set; }
         public string Status { get; set; }
 
